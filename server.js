b401d2811919ba085517ee5c779ba7a99d59e387
sumanth <84782657+rakurthisumanth@users.noreply.github.com>
2025-04-24 14:18:07 +0530

    backend_

--- server.js
+++ server.js
@@ -1,34 +1,57 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
 app.use(cors());
 
-io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.id}`);
-    
-    socket.on('send_message', (data) => {
-        console.log(data,"server resposne");
-        io.emit('received',data)
-    });
+// Store users
+const users = {}; // { username: socket.id }
+const socketToUser = {}; // { socket.id: username }
 
-    socket.on('disconnect', () => {
-        console.log(`User disconnected: ${socket.id}`);
-    });
+io.on('connection', (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  // Login handler
+  socket.on('login', (username) => {
+    users[username] = socket.id;
+    socketToUser[socket.id] = username;
+    console.log(`${username} logged in`);
+
+    // Send updated user list to everyone
+    io.emit('users_online', Object.keys(users));
+  });
+
+  // Handle private message
+  socket.on('send_private_message', ({ to, from, message }) => {
+    const targetSocketId = users[to];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit('private_message', { from, message });
+    }
+  });
+
+  // On disconnect
+  socket.on('disconnect', () => {
+    const username = socketToUser[socket.id];
+    if (username) {
+      delete users[username];
+      delete socketToUser[socket.id];
+      console.log(`${username} disconnected`);
+      io.emit('users_online', Object.keys(users));
+    }
+  });
 });
 
-// Use `server.listen` instead of `app.listen`
 server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server listening on http://localhost:3000');
 });
